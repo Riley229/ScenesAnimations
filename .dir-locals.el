90a66d6573f54c75ad51acdfdad5418a14f9e7fa
David Ben-Yaakov <david@tangogolfdigital.com>
2020-10-09 14:06:07 -0500

    Bumped version to Igis 1.3.5 and Scenes 1.1.1

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((swift-mode . 
-  ((lsp-sourcekit-extra-args . (
-	 "-Xlinker" "-L" "-Xlinker" "/home/camden-thomson/Scenes/Igis/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/home/camden-thomson/Scenes/Igis/.build/debug" 
-	 "-Xlinker" "-L" "-Xlinker" "/home/camden-thomson/Scenes/Scenes/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/home/camden-thomson/Scenes/Scenes/.build/debug" 
-
-)))))
